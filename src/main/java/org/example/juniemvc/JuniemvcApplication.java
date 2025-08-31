5004d8d02d4cf5da0dd72066c4b0ddec56e225ea
bastias <jbastiasm@gmail.com>
2025-08-31 13:22:57 +0200

    Init

---
+++ src/main/java/org/example/juniemvc/JuniemvcApplication.java
@@ -0,0 +1,13 @@
+package org.example.juniemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniemvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuniemvcApplication.class, args);
+    }
+
+}
